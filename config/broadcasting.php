da17fb4e2c412f2551196c090f6401b1a86d3ec4
Leo <silently0801@gmail.com>
2025-07-15 11:44:04 +0800

    no message

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -47,12 +47,12 @@ return [
         ],
 
         'pusher' => [
-            'driver' => 'pusher',
-            'key' => env('PUSHER_APP_KEY', ''),
-            'secret' => env('PUSHER_APP_SECRET', ''),
-            'app_id' => env('PUSHER_APP_ID', ''),
+            'driver' => env('PUSHER_APP_KEY') ? 'pusher' : 'null',
+            'key' => env('PUSHER_APP_KEY') ?: '',
+            'secret' => env('PUSHER_APP_SECRET') ?: '',
+            'app_id' => env('PUSHER_APP_ID') ?: '',
             'options' => [
-                'cluster' => env('PUSHER_APP_CLUSTER', ''),
+                'cluster' => env('PUSHER_APP_CLUSTER', 'mt1'),
                 'host' => env('PUSHER_HOST') ?: 'api-'.env('PUSHER_APP_CLUSTER', 'mt1').'.pusher.com',
                 'port' => env('PUSHER_PORT', 443),
                 'scheme' => env('PUSHER_SCHEME', 'https'),

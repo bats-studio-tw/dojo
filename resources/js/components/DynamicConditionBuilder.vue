1fa60f37b99e578b9ed144689edffcb4c2f4e5e2
silently0801 <silently0801@gmail.com>
2025-07-17 21:56:21 +0800

    no message

--- resources/js/components/DynamicConditionBuilder.vue
+++ resources/js/components/DynamicConditionBuilder.vue
@@ -45,7 +45,7 @@
                   { label: 'OR', value: 'or' }
                 ]"
                 size="tiny"
-                class="w-20"
+                class="!w-20"
                 @update:value="updateConditionLogic(condition.id, $event)"
               />
             </div>
